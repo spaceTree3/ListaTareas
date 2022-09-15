2fba1a0c1aef839c43a79d5d967ab5eda880a05d
spaceTree3 <j_sotom@hotmail.es>
2022-09-15 19:47:54 -0300

    Agregar el mock de tareas e interface de las mismas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import { Task } from "./task"
+export const TASKS:Task[]= [
+    {
+        id: 1,
+        text:"Terminar el primer modulo de angular",
+        day:"Agosto 5 a las 12:00",
+        reminder:true
+    },
+    {
+        id: 2,
+        text:"Hacer las compras para la cena",
+        day:"Agosto 5 a las 12:00",
+        reminder:true
+    },
+    {
+        id: 3,
+        text:"Investigar sobre bootstrap",
+        day:"Agosto 5 a las 12:00",
+        reminder:false
+    },
+    {
+        id: 4,
+        text:"Leer mi libro favorito",
+        day:"Agosto 5 a las 12:00",
+        reminder:false
+    }
+]
