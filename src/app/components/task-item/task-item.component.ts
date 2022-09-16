65926b12d811ca46ebc7e166880e588f6a96a74b
spaceTree3 <j_sotom@hotmail.es>
2022-09-16 00:09:47 -0300

    componente de lista de items

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { Task } from 'src/app/task';
 import { TASKS } from 'src/app/mock-task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 @Component({
   selector: 'app-task-item',
@@ -9,6 +13,8 @@ import { TASKS } from 'src/app/mock-task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task:Task=TASKS[0];
+  faTimes = faTimes;
+  
 
   constructor() { }
 
